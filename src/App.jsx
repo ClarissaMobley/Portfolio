4f55344262e16471ff68e72207b92886184b5562
Clarissa Mobley <clarissamobley@me.com>
2024-07-16 14:39:58 -0400

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import Banner from './components/Banner'
 import Navbar from './components/Navbar'
 import Header from './components/Header'
 import About from './components/About'
@@ -10,7 +9,6 @@ import Contact from './components/Contact'
 function App() {
   return (
    <div>
-    <Banner />
     <Navbar />
     <Header />
     <About />
@@ -22,3 +20,11 @@ function App() {
 }
 
 export default App
+
+
+// secondary color #e4b3b3
+// potential 2nd #b3b3e4
+// potential 3rd #cbb3e4
+// potential 4th #b3e4e4
+// potential 5th ##b3e4b3
+// potential 6th #714091
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <nav className='bg-custom text-white'>navbar</nav>
+        
+        <nav className='bg-custom text-white px-8 md:px-16 lg:px-10'>
+            <div className='container mx-auto py-2 flex justify-center items-center'>
+                <div className='flex w-full justify-around'>
+                    <a href="#home" className="hover:text-[#e4b3b3]">Home</a>
+                    <a href="#about" className="hover:text-[#e4b3b3]">About Me</a>
+                    <a href="#projects" className="hover:text-[#e4b3b3]">Projects</a>
+                    <a href="#resume" className="hover:text-[#e4b3b3]">Resume</a>
+                    <a href="#contact" className="hover:text-[#e4b3b3]">Contact Me</a>
+                </div>
+            </div>
+        </nav>
+     
     )
 }
 
