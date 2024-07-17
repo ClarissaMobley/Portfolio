57255d72da6eb13fa0a055b2263a33badfa1f632
Clarissa Mobley <clarissamobley@me.com>
2024-07-17 17:31:20 -0400

    updated contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,42 +1,50 @@
 import React from 'react';
 
+
 const Contact = () => {
     return (
-        <div className='text-white py-20' id='contact'>
+        <div className='py-20' id='contact'>
             <div className='container mx-auto px-8 md:px-16 lg:px-24'>
-                <h3 className='text-4xl font-bold text-center mb-12'>Let's get in touch!</h3>
+                <h3 className='text-4xl font-bold text-center mb-12 text-white'>Let's get in touch!</h3>
                 <div className='flex justify-center'>
                     <div className="w-full max-w-lg">
                         <form className="space-y-4">
-                            {['name', 'email', 'message'].map((field, index) => (
-                                <div key={index}>
-                                    <label htmlFor={field} className="block mb-2 text-xl">
-                                        {field.charAt(0).toUpperCase() + field.slice(1)}
-                                    </label>
-                                    {field === 'message' ? (
-                                        <textarea
-                                            id={field}
-                                            name={field}
-                                            className="w-full p-2 rounded bg-white border border-[#e4b3b3] focus:outline-none focus:border-[#e4b3b3]"
-                                            rows="5"
-                                            placeholder={field.charAt(0).toUpperCase() + field.slice(1)}
-                                            aria-label={field.charAt(0).toUpperCase() + field.slice(1)}
-                                        />
-                                    ) : (
-                                        <input
-                                            type={field === 'email' ? 'email' : 'text'}
-                                            id={field}
-                                            name={field}
-                                            className="w-full p-2 rounded bg-white border border-[#e4b3b3] focus:outline-none focus:border-[#e4b3b3]"
-                                            placeholder={field.charAt(0).toUpperCase() + field.slice(1)}
-                                            aria-label={field.charAt(0).toUpperCase() + field.slice(1)}
-                                        />
-                                    )}
-                                </div>
-                            ))}
+                            <div>
+                                <label htmlFor="name" className="text-white block mb-2 text-xl">Your Name</label>
+                                <input 
+                                    type="text" 
+                                    id="name" 
+                                    name="name"
+                                    className="w-full p-2 rounded bg-white border border-[#e4b3b3] focus:outline-none focus:border-[#e4b3b3]" 
+                                    placeholder="Name" 
+                                    aria-label="Your Name"
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="email" className="text-white block mb-2 text-xl">Your Email</label>
+                                <input 
+                                    type="email" 
+                                    id="email" 
+                                    name="email"
+                                    className="w-full p-2 rounded bg-white border border-[#e4b3b3] focus:outline-none focus:border-[#e4b3b3]" 
+                                    placeholder="Email" 
+                                    aria-label="Your Email"
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="message" className="text-white block mb-2 text-xl">Message</label>
+                                <textarea 
+                                    id="message" 
+                                    name="message"
+                                    className="w-full p-2 rounded bg-white border border-[#e4b3b3] focus:outline-none focus:border-[#e4b3b3]" 
+                                    rows='5' 
+                                    placeholder="Message" 
+                                    aria-label="Message"
+                                />
+                            </div>
                             <button 
                                 type="submit"
-                                className="bg-gradient-to-r from-[#e4b3b3] to-blue-500 text-white font-bold transform transition-transform duration-300 hover:scale-105 px-8 py-2 rounded-lg w-full text-xl"
+                                className="btn bg-[#b77676] text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-8 py-2 rounded-lg w-full text-xl"
                             >
                                 Send
                             </button>
