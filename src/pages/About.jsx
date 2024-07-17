6ea5df0b6ba491238f8e4423f5a98042121f8c80
Clarissa Mobley <clarissamobley@me.com>
2024-07-17 09:40:16 -0400

    did styling changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,18 +10,18 @@ const About = () => {
                 <div className='flex flex-col md:flex-row items-center md:space-x-12 animate-fadeIn'>
                     <img src={AboutMeImage} alt="" className='w-96 mb-8 md:mb-0 hover:scale-105' />
                     <div className='flex-1'>
-                        <h2 className='text-6xl text-[#e4b3b3] font-bold text-start mb-8'>About Me</h2>
+                        <h2 className='text-6xl text-[#e4b3b3] font-bold text-start mb-6'>About Me</h2>
                         <h3 className='text-2xl text-white font-bold'>I'm Clarissa Mobley, a Full-Stack Developer based in Atlanta.</h3>
-                        <p className='text-md mb-4'> I have a solid foundation in HTML, CSS, and JavaScript. Before transitioning into tech, 
+                        <p className='text-sm mb-4'> I have a solid foundation in HTML, CSS, and JavaScript. Before transitioning into tech, 
                             I spent 11 years in the set decoration department for films, where I developed a strong eye for visual storytelling 
                             and attention to detail. This experience helped shape my approach to web development, enabling me to create applications 
                             that are both visually appealing and functional.
                         </p>
-                        <p className='text-md mb-4'> In my journey to become a developer, I have learned to work with a variety of technologies, 
+                        <p className='text-sm mb-4'> In my journey to become a developer, I have learned to work with a variety of technologies, 
                             including the MERN stack (MongoDB, Express, React, and Node.js). This has allowed me to build full-stack applications that 
                             are responsive, scalable, and user-friendly. I'm also always eager to learn new technologies and expand my skill set.
                         </p>
-                        <p className='text-md mb-4'>
+                        <p className='text-sm mb-4'>
                             I enjoy the entire process of web development, from concept to deployment. As I continue to expand my skills and explore new 
                             technologies, I'm committed to solving complex problems and delivering solutions. Whether it's creating RESTful APIs, implementing 
                             responsive design, or optimizing database performance, I'm focused on making sure that I meet and exceed the needs of users.
