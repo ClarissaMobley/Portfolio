5306a0a841beff354201d7638f87d16f63681dac
Clarissa Mobley <clarissamobley@me.com>
2024-07-18 10:58:05 -0400

    added contact to server.js

--- server.js
+++ server.js
@@ -6,3 +6,25 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const contactSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    message: String,
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+app.post('/contact', async(req, res) => {
+    const { name, email, message } = req.body;
+    const contact = new Contact({ name, email, message });
+
+    try {
+        await contact.save();
+    }
+})
