b00103f706fe3a1873dfe9bccce453ba44685bfb
Clarissa Mobley <clarissamobley@me.com>
2024-07-18 20:26:18 -0400

    added to api

--- routes/contact.js
+++ routes/contact.js
@@ -4,7 +4,7 @@ import Contact from '../models/contact.js';
 
 const router = express.Router();
 
-router.post('/contact', async (req, res) => {
+router.post('/api/contact', async (req, res) => {
   const { name, email, message } = req.body;
   const contact = new Contact({ name, email, message });
 
