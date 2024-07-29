3b5d7d716e672cf0a0dbdfb21984295a315bc1f9
Clarissa Mobley <clarissamobley@me.com>
2024-07-28 23:24:27 -0400

    update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import contactRoutes from './routes/contact.js';
 const app = express();
 
 const corsOptions = {
-  origin: process.env.VITE_BASE_URL,
+  origin: process.env.VITE_BASE_URL, process.env.VITE_API_URL,
   methods: 'GET, POST, OPTIONS', 
   allowedHeaders: 'Content-Type, Authorization',
   optionsSuccessStatus: 204 
